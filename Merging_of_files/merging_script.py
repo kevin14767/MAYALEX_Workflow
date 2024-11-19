b6a4f078b6514319a874a969479f809f7422379c
Kevin Barcenas <kevinbarcenas2022@gmail.com>
2024-11-18 18:08:13 -0600

    Finished updating Maya's spelling changes from her file into the master file, need to update parts of speech

---
+++ Merging_of_files/merging_script.py
@@ -0,0 +1,5 @@
+from IPython.display import display
+import pandas as pd
+
+second_file = pd.read_excel("./Merging_of_files/Mayas_file.xlsx")
+first_file = pd.read_excel("./Merging_of_files/First_file.xlsx")
